20514c2b2f3c5d530e65d546d179fb1aca351e21
Manh Nguyen <manhnd9293@gmail.com>
2024-10-12 17:12:36 +0700

    build: init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </h1>
+  )
+}
+
+export default App
